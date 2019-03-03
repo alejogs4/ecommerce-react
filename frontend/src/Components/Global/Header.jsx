dea71215f98340d26393105f7a2528cb8a162ccb
Mauricio Garzon <41930602+maugarbru@users.noreply.github.com>
2019-03-03 16:40:48 -0500

    Diseño Header(NavBar)

--- frontend/src/Components/Global/Header.jsx
+++ frontend/src/Components/Global/Header.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import '../Styles/header.css'
+import { Link } from 'react-router-dom'
+import Nombre from '../../Images/NombreBLANCO.png';
+import Logo from '../../Images/LogoPNG.png';
+
 const Header = () => (
-  <header>
-    hola
-  </header>
+  <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src={Nombre} alt="Nombre" width="112" height="28" />
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" href="/">
+         <img src={Logo} alt='Logo' width="34" height="34"/>
+        </a>
+      </div>
+    </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-black">
+              <Link to='/signup'>SignUp</Link>
+            </a>
+            <a class="button is-black">
+              <Link to='/login'>LogIn</Link>
+            </a>
+          </div>
+        </div>
+      </div>
+  </nav>
 )
 export default Header
