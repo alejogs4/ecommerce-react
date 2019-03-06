503704d1a5bf832aea6de76df0efeae6152ed7a7
Miguel Vélez <34169959+mvelezg99@users.noreply.github.com>
2019-03-06 18:11:40 -0500

    problems

--- frontend/src/Components/Pages/ShoppingCart.jsx
+++ frontend/src/Components/Pages/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { deleteItem, addItem } from "../../Helpers/handleLocalStorage"
+import { withRouter } from 'react-router-dom'
 
 import ProductTableFieldShopping from '../Atoms/ProductsTableShopping'
 import ProductTableFieldCart from '../Atoms/ProductsTableCart'
@@ -35,45 +36,66 @@ class ShoppingCart extends Component {
     }
   }
 
+  handleBuy = () => {
+    return () => {
+      localStorage.setItem('shopping', '[]')
+      this.setState(prev => ({
+        shopping: [],
+        totalPrice: 0
+      }))
+      this.history.props.push("/")
+    }
+    
+
+
+
+  }
+
 
   render() {
     const { shopping, totalPrice, products } = this.state
 
     return (
       <main>
-        <br/>
+        <br />
         <section className="hero is-black is-fullheight">
           <div className="hero-body">
             <div className="container has-text-centered">
               <h3 className="title has-text-white">Shopping Cart</h3>
-                <div className="container">
-                  <div className="notification">
-                    {shopping.map((item, key) => (
-                      <ProductTableFieldCart key={key}
-                        item={item}
-                        handleDelete={this.handleDelete(item)} />
-                    ))}
-                  </div>
+              <div className="container">
+                <div className="notification">
+                  {shopping.map((item, key) => (
+                    <ProductTableFieldCart key={key}
+                      item={item}
+                      handleDelete={this.handleDelete(item)} />
+                  ))}
                 </div>
-                <br/>
-                <h2 className="title has-text-grey">Total price: {totalPrice}</h2  >
-                <br/>
-                <h3 className="title has-text-white">Products List</h3>               
-                <div className="container">
-                  <div className="notification">
-                    {products.map((item, key) => (
-                      <ProductTableFieldShopping key={key}
-                        item={item}
-                        handleAdd={this.handleAdd(item)} />
-                    ))}
-                  </div>
+              </div>
+              <br />
+              <h2 className="title has-text-grey">Total price: {totalPrice}</h2  >
+              <button onClick={this.handleBuy} className="button is-success is-fullwidth">Buy</button>
+              <br />
+              <h3 className="title has-text-white">Products List</h3>
+              <div className="container">
+                <div className="notification">
+                  {products.map((item, key) => (
+                    <ProductTableFieldShopping key={key}
+                      item={item}
+                      handleAdd={this.handleAdd(item)} />
+                  ))}
                 </div>
+
+              </div>
+
             </div>
+
           </div>
+
         </section>
+
       </main>
     )
   }
 }
 
-export default ShoppingCart
+export default withRouter(ShoppingCart)
